a5251f6ae89b564549a07d668129aaf774e4e1f3
Peter Epley <PAE2008@gmail.com>
2021-10-13 23:38:37 -0400

    2021.10.4

--- custom_components/ble_monitor/config_flow.py
+++ custom_components/ble_monitor/config_flow.py
@@ -24,7 +24,6 @@ from .const import (
     DEFAULT_LOG_SPIKES,
     DEFAULT_USE_MEDIAN,
     DEFAULT_ACTIVE_SCAN,
-    DEFAULT_BATT_ENTITIES,
     DEFAULT_REPORT_UNKNOWN,
     DEFAULT_DISCOVERY,
     DEFAULT_RESTORE_STATE,
@@ -32,14 +31,16 @@ from .const import (
     DEFAULT_DEVICE_USE_MEDIAN,
     DEFAULT_DEVICE_RESTORE_STATE,
     DEFAULT_DEVICE_RESET_TIMER,
+    DEFAULT_DEVICE_TRACK,
+    DEFAULT_DEVICE_TRACKER_SCAN_INTERVAL,
+    DEFAULT_DEVICE_TRACKER_CONSIDER_HOME,
+    DEFAULT_DEVICE_DELETE_DEVICE,
     CONF_DECIMALS,
     CONF_PERIOD,
     CONF_LOG_SPIKES,
     CONF_USE_MEDIAN,
     CONF_ACTIVE_SCAN,
-    CONF_HCI_INTERFACE,
     CONF_BT_INTERFACE,
-    CONF_BATT_ENTITIES,
     CONF_REPORT_UNKNOWN,
     CONF_RESTORE_STATE,
     CONF_ENCRYPTION_KEY,
@@ -47,10 +48,15 @@ from .const import (
     CONF_DEVICE_USE_MEDIAN,
     CONF_DEVICE_RESTORE_STATE,
     CONF_DEVICE_RESET_TIMER,
+    CONF_DEVICE_TRACK,
+    CONF_DEVICE_TRACKER_SCAN_INTERVAL,
+    CONF_DEVICE_TRACKER_CONSIDER_HOME,
+    CONF_DEVICE_DELETE_DEVICE,
     CONFIG_IS_FLOW,
     DOMAIN,
     MAC_REGEX,
-    AES128KEY_REGEX,
+    AES128KEY24_REGEX,
+    AES128KEY32_REGEX,
 )
 
 from . import (
@@ -76,17 +82,28 @@ DEVICE_SCHEMA = vol.Schema(
         vol.Optional(CONF_DEVICE_DECIMALS, default=DEFAULT_DEVICE_DECIMALS): vol.In(
             [DEFAULT_DEVICE_DECIMALS, 0, 1, 2, 3]
         ),
-        vol.Optional(
-            CONF_DEVICE_USE_MEDIAN, default=DEFAULT_DEVICE_USE_MEDIAN): vol.In(
+        vol.Optional(CONF_DEVICE_USE_MEDIAN, default=DEFAULT_DEVICE_USE_MEDIAN): vol.In(
             [DEFAULT_DEVICE_USE_MEDIAN, True, False]
         ),
         vol.Optional(
-            CONF_DEVICE_RESTORE_STATE, default=DEFAULT_DEVICE_RESTORE_STATE): vol.In(
-            [DEFAULT_DEVICE_RESTORE_STATE, True, False]
-        ),
+            CONF_DEVICE_RESTORE_STATE, default=DEFAULT_DEVICE_RESTORE_STATE
+        ): vol.In([DEFAULT_DEVICE_RESTORE_STATE, True, False]),
         vol.Optional(
             CONF_DEVICE_RESET_TIMER, default=DEFAULT_DEVICE_RESET_TIMER
         ): cv.positive_int,
+        vol.Optional(CONF_DEVICE_TRACK, default=DEFAULT_DEVICE_TRACK): cv.boolean,
+        vol.Optional(
+            CONF_DEVICE_TRACKER_SCAN_INTERVAL,
+            default=DEFAULT_DEVICE_TRACKER_SCAN_INTERVAL,
+        ): cv.positive_int,
+        vol.Optional(
+            CONF_DEVICE_TRACKER_CONSIDER_HOME,
+            default=DEFAULT_DEVICE_TRACKER_CONSIDER_HOME,
+        ): cv.positive_int,
+        vol.Optional(
+            CONF_DEVICE_DELETE_DEVICE,
+            default=DEFAULT_DEVICE_DELETE_DEVICE,
+        ): cv.boolean,
     }
 )
 
@@ -98,14 +115,31 @@ DOMAIN_SCHEMA = vol.Schema(
         vol.Optional(CONF_PERIOD, default=DEFAULT_PERIOD): cv.positive_int,
         vol.Optional(CONF_DISCOVERY, default=DEFAULT_DISCOVERY): cv.boolean,
         vol.Optional(CONF_ACTIVE_SCAN, default=DEFAULT_ACTIVE_SCAN): cv.boolean,
-        vol.Optional(CONF_REPORT_UNKNOWN, default=DEFAULT_REPORT_UNKNOWN): cv.boolean,
-        vol.Optional(CONF_BATT_ENTITIES, default=DEFAULT_BATT_ENTITIES): cv.boolean,
         vol.Optional(CONF_DECIMALS, default=DEFAULT_DECIMALS): cv.positive_int,
         vol.Optional(CONF_LOG_SPIKES, default=DEFAULT_LOG_SPIKES): cv.boolean,
         vol.Optional(CONF_USE_MEDIAN, default=DEFAULT_USE_MEDIAN): cv.boolean,
         vol.Optional(CONF_RESTORE_STATE, default=DEFAULT_RESTORE_STATE): cv.boolean,
         vol.Optional(CONF_DEVICES, default=[]): vol.All(
             cv.ensure_list, [DEVICE_SCHEMA]
+        ),
+        vol.Optional(CONF_REPORT_UNKNOWN, default=DEFAULT_REPORT_UNKNOWN): vol.In(
+            [
+                "ATC",
+                "Brifit",
+                "Govee",
+                "iNode",
+                "Kegtron",
+                "Mi Scale",
+                "Moat",
+                "Qingping",
+                "Ruuvitag",
+                "SensorPush",
+                "Teltonika",
+                "Thermoplus",
+                "Xiaomi",
+                "Other",
+                False,
+            ]
         ),
     }
 )
@@ -135,8 +169,9 @@ class BLEMonitorFlow(data_entry_flow.FlowHandler):
         """Key validation."""
         if not value or value == "-":
             return
-        if not self.validate_regex(value, AES128KEY_REGEX):
-            errors[CONF_ENCRYPTION_KEY] = "invalid_key"
+        if not self.validate_regex(value, AES128KEY24_REGEX):
+            if not self.validate_regex(value, AES128KEY32_REGEX):
+                errors[CONF_ENCRYPTION_KEY] = "invalid_key"
 
     def _show_main_form(self, errors=None):
         _LOGGER.error("_show_main_form: shouldn't be here")
@@ -176,14 +211,16 @@ class BLEMonitorFlow(data_entry_flow.FlowHandler):
             step_id=step_id, data_schema=config_schema, errors=errors or {}
         )
 
-    async def async_step_add_device(self, user_input=None):
-        """Add device step."""
+    async def async_step_add_remove_device(self, user_input=None):
+        """Add/remove device step."""
         errors = {}
         if user_input is not None:
-            _LOGGER.debug("async_step_add_device: %s", user_input)
-            if user_input[CONF_MAC] and user_input[CONF_MAC] != "-":
+            _LOGGER.debug("async_step_add_remove_device: %s", user_input)
+            if user_input[CONF_MAC] and not user_input[CONF_DEVICE_DELETE_DEVICE]:
                 if (
-                    self._sel_device and user_input[CONF_MAC].upper() != self._sel_device.get(CONF_MAC).upper()
+                    self._sel_device
+                    and user_input[CONF_MAC].upper()
+                    != self._sel_device.get(CONF_MAC).upper()
                 ):
                     errors[CONF_MAC] = "cannot_change_mac"
                     user_input[CONF_MAC] = self._sel_device.get(CONF_MAC)
@@ -228,15 +265,40 @@ class BLEMonitorFlow(data_entry_flow.FlowHandler):
                             CONF_DEVICE_RESET_TIMER,
                             default=user_input[CONF_DEVICE_RESET_TIMER],
                         ): cv.positive_int,
+                        vol.Optional(
+                            CONF_DEVICE_TRACK,
+                            default=user_input[CONF_DEVICE_TRACK],
+                        ): cv.boolean,
+                        vol.Optional(
+                            CONF_DEVICE_TRACKER_SCAN_INTERVAL,
+                            default=user_input[CONF_DEVICE_TRACKER_SCAN_INTERVAL],
+                        ): cv.positive_int,
+                        vol.Optional(
+                            CONF_DEVICE_TRACKER_CONSIDER_HOME,
+                            default=user_input[CONF_DEVICE_TRACKER_CONSIDER_HOME],
+                        ): cv.positive_int,
+                        vol.Optional(
+                            CONF_DEVICE_DELETE_DEVICE,
+                            default=DEFAULT_DEVICE_DELETE_DEVICE,
+                        ): cv.boolean,
                     }
                 )
                 return self.async_show_form(
-                    step_id="add_device",
+                    step_id="add_remove_device",
                     data_schema=retry_device_option_schema,
                     errors=errors,
                 )
             if self._sel_device:
-                del self._devices[self._sel_device.get(CONF_MAC).upper()]
+                """Remove device from device registry."""
+                device_registry = await self.hass.helpers.device_registry.async_get_registry()
+                mac = self._sel_device.get(CONF_MAC).upper()
+                device = device_registry.async_get_device({(DOMAIN, mac)}, set())
+                if device is None:
+                    errors[CONF_MAC] = "cannot_delete_device"
+                else:
+                    _LOGGER.debug("Removing BLE monitor device %s", mac)
+                    device_registry.async_remove_device(device.id)
+                    del self._devices[self._sel_device.get(CONF_MAC).upper()]
             return self._show_main_form(errors)
         device_option_schema = vol.Schema(
             {
@@ -280,13 +342,35 @@ class BLEMonitorFlow(data_entry_flow.FlowHandler):
                     CONF_DEVICE_RESET_TIMER,
                     default=self._sel_device.get(CONF_DEVICE_RESET_TIMER)
                     if self._sel_device.get(CONF_DEVICE_RESET_TIMER)
-                    else DEFAULT_DEVICE_RESET_TIMER
+                    else DEFAULT_DEVICE_RESET_TIMER,
                 ): cv.positive_int,
+                vol.Optional(
+                    CONF_DEVICE_TRACK,
+                    default=self._sel_device.get(CONF_DEVICE_TRACK)
+                    if self._sel_device.get(CONF_DEVICE_TRACK)
+                    else DEFAULT_DEVICE_TRACK,
+                ): cv.boolean,
+                vol.Optional(
+                    CONF_DEVICE_TRACKER_SCAN_INTERVAL,
+                    default=self._sel_device.get(CONF_DEVICE_TRACKER_SCAN_INTERVAL)
+                    if self._sel_device.get(CONF_DEVICE_TRACKER_SCAN_INTERVAL)
+                    else DEFAULT_DEVICE_TRACKER_SCAN_INTERVAL,
+                ): cv.positive_int,
+                vol.Optional(
+                    CONF_DEVICE_TRACKER_CONSIDER_HOME,
+                    default=self._sel_device.get(CONF_DEVICE_TRACKER_CONSIDER_HOME)
+                    if self._sel_device.get(CONF_DEVICE_TRACKER_CONSIDER_HOME)
+                    else DEFAULT_DEVICE_TRACKER_CONSIDER_HOME,
+                ): cv.positive_int,
+                vol.Optional(
+                    CONF_DEVICE_DELETE_DEVICE,
+                    default=DEFAULT_DEVICE_DELETE_DEVICE,
+                ): cv.boolean,
             }
         )
 
         return self.async_show_form(
-            step_id="add_device",
+            step_id="add_remove_device",
             data_schema=device_option_schema,
             errors=errors,
         )
@@ -320,10 +404,10 @@ class BLEMonitorConfigFlow(BLEMonitorFlow, config_entries.ConfigFlow, domain=DOM
                 user_input[CONF_DEVICES] = {}
             elif user_input[CONF_DEVICES] == OPTION_ADD_DEVICE:
                 self._sel_device = {}
-                return await self.async_step_add_device()
+                return await self.async_step_add_remove_device()
             if user_input[CONF_DEVICES] in self._devices:
                 self._sel_device = self._devices[user_input[CONF_DEVICES]]
-                return await self.async_step_add_device()
+                return await self.async_step_add_remove_device()
             await self.async_set_unique_id(DOMAIN_TITLE)
             self._abort_if_unique_id_configured()
             return self._create_entry(user_input)
@@ -356,9 +440,7 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
                 ): cv.multi_select(BT_MAC_INTERFACES),
                 vol.Optional(
                     CONF_PERIOD,
-                    default=self.config_entry.options.get(
-                        CONF_PERIOD, DEFAULT_PERIOD
-                    ),
+                    default=self.config_entry.options.get(CONF_PERIOD, DEFAULT_PERIOD),
                 ): cv.positive_int,
                 vol.Optional(
                     CONF_DISCOVERY,
@@ -370,18 +452,6 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
                     CONF_ACTIVE_SCAN,
                     default=self.config_entry.options.get(
                         CONF_ACTIVE_SCAN, DEFAULT_ACTIVE_SCAN
-                    ),
-                ): cv.boolean,
-                vol.Optional(
-                    CONF_REPORT_UNKNOWN,
-                    default=self.config_entry.options.get(
-                        CONF_REPORT_UNKNOWN, DEFAULT_REPORT_UNKNOWN
-                    ),
-                ): cv.boolean,
-                vol.Optional(
-                    CONF_BATT_ENTITIES,
-                    default=self.config_entry.options.get(
-                        CONF_BATT_ENTITIES, DEFAULT_BATT_ENTITIES
                     ),
                 ): cv.boolean,
                 vol.Optional(
@@ -408,6 +478,30 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
                         CONF_RESTORE_STATE, DEFAULT_RESTORE_STATE
                     ),
                 ): cv.boolean,
+                vol.Optional(
+                    CONF_REPORT_UNKNOWN,
+                    default=self.config_entry.options.get(
+                        CONF_REPORT_UNKNOWN, DEFAULT_REPORT_UNKNOWN
+                    ),
+                ): vol.In(
+                    [
+                        "ATC",
+                        "Brifit",
+                        "Govee",
+                        "iNode",
+                        "Kegtron",
+                        "Mi Scale",
+                        "Moat",
+                        "Qingping",
+                        "Ruuvitag",
+                        "SensorPush",
+                        "Teltonika",
+                        "Thermoplus",
+                        "Xiaomi",
+                        "Other",
+                        False,
+                    ]
+                ),
             }
         )
         return self._show_user_form("init", options_schema, errors or {})
@@ -421,20 +515,22 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
         if user_input is not None:
             _LOGGER.debug("async_step_init (after): %s", user_input)
             if (
-                CONFIG_IS_FLOW in self.config_entry.options and not self.config_entry.options[CONFIG_IS_FLOW]
+                CONFIG_IS_FLOW in self.config_entry.options
+                and not self.config_entry.options[CONFIG_IS_FLOW]
             ):
                 return self.async_abort(reason="not_in_use")
             if user_input[CONF_DEVICES] == OPTION_ADD_DEVICE:
                 self._sel_device = {}
-                return await self.async_step_add_device()
+                return await self.async_step_add_remove_device()
             if user_input[CONF_DEVICES] in self._devices:
                 self._sel_device = self._devices[user_input[CONF_DEVICES]]
-                return await self.async_step_add_device()
+                return await self.async_step_add_remove_device()
             return self._create_entry(user_input)
         _LOGGER.debug("async_step_init (before): %s", self.config_entry.options)
 
         if (
-            CONFIG_IS_FLOW in self.config_entry.options and not self.config_entry.options[CONFIG_IS_FLOW]
+            CONFIG_IS_FLOW in self.config_entry.options
+            and not self.config_entry.options[CONFIG_IS_FLOW]
         ):
             options_schema = vol.Schema({vol.Optional("not_in_use", default=""): str})
             return self.async_show_form(
@@ -443,7 +539,9 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
         for dev in self.config_entry.options.get(CONF_DEVICES):
             self._devices[dev[CONF_MAC].upper()] = dev
         devreg = await self.hass.helpers.device_registry.async_get_registry()
-        for dev in device_registry.async_entries_for_config_entry(devreg, self.config_entry.entry_id):
+        for dev in device_registry.async_entries_for_config_entry(
+            devreg, self.config_entry.entry_id
+        ):
             for iddomain, idmac in dev.identifiers:
                 if iddomain != DOMAIN:
                     continue
@@ -454,7 +552,9 @@ class BLEMonitorOptionsFlow(BLEMonitorFlow, config_entries.OptionsFlow):
                     self._devices[idmac] = {CONF_MAC: idmac, CONF_NAME: name}
 
         # sort devices by name
-        sorteddev_tuples = sorted(self._devices.items(), key=lambda item: item[1].get("name", item[1]["mac"]))
+        sorteddev_tuples = sorted(
+            self._devices.items(), key=lambda item: item[1].get("name", item[1]["mac"])
+        )
         self._devices = dict(sorteddev_tuples)
         self.hass.config_entries.async_update_entry(
             self.config_entry, unique_id=self.config_entry.entry_id
--- custom_components/camect/camera.py
+++ custom_components/camect/camera.py
@@ -13,32 +13,40 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'camect'
 
 
-def setup_platform(hass, config, add_entities, cam_ids):
+def setup_platform(hass, config, add_entities, data):
     """Add an entity for every camera from Camect Home."""
     component = hass.data[camera.DOMAIN]
     hass.http.register_view(CamectWebsocketView(component))
 
-    home = hass.data[DOMAIN]
-    camect_site = home.get_cloud_url('')
-    cam_jsons = home.list_cameras()
-    if cam_jsons:
-        cams = []
-        for cj in cam_jsons:
-            if not cam_ids or cj['id'] in cam_ids:
-                cams.append(Camera(home, cj, camect_site))
-        add_entities(cams, True)
+    cams = []
+    homes = hass.data[DOMAIN]
+    for i, home_data in enumerate(data):
+        home = homes[i]
+        camect_site = home.get_cloud_url('')
+        cam_jsons = home.list_cameras()
+        cam_ids = home_data[0]
+        home_id = home_data[1]
+        if cam_jsons:
+            for cj in cam_jsons:
+                if not cam_ids or cj['id'] in cam_ids:
+                    cams.append(Camera(home_id, home, cj, camect_site))
+    add_entities(cams, True)
     return True
 
 
 class Camera(camera.Camera):
     """An implementation of a camera supported by Camect Home."""
 
-    def __init__(self, home, json: Dict[str, str], camect_site: str):
+    def __init__(self, home_id: str, home, json: Dict[str, str], camect_site: str):
         """Initialize a camera supported by Camect Home."""
         super(Camera, self).__init__()
+        self._home_id = home_id
         self._home = home
         self._device_id = json['id']
-        self._id = '{}_{}'.format(DOMAIN, self._device_id)
+        if self._home_id:
+            self._id = '{}_{}{}'.format(DOMAIN, self._home_id, self._device_id)
+        else:
+            self._id = '{}_{}'.format(DOMAIN, self._device_id)
         self.entity_id = '{}.{}'.format(camera.DOMAIN, self._id)
         self._name = json['name']
         self._make = json['make'] or ''
@@ -104,6 +112,9 @@ class Camera(camera.Camera):
         """No need for the poll."""
         return False
 
+    def home(self):
+        return self._home
+
 
 class CamectWebsocketView(camera.CameraView):
     """Camect view to proxy Websocket to home."""
@@ -116,8 +127,7 @@ class CamectWebsocketView(camera.CameraView):
         ha_ws = web.WebSocketResponse()
         await ha_ws.prepare(request)
 
-        hass = request.app['hass']
-        home = hass.data[DOMAIN]
+        home = camera.home()
         ws_url = home.get_unsecure_websocket_url()
         if not ws_url:
             raise web.HTTPInternalServerError()
